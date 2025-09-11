54d61d3d62b9faa1fe1cc322a0621b84111c38df
vavedev <vave.gamedev@gmail.com>
2025-09-11 22:24:07 +0800

    Initial commit

---
+++ src/PeakNoiseSupression/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace PeakNoiseSupression;
+
+// Here are some basic resources on code style and naming conventions to help
+// you in your first CSharp plugin!
+// https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/coding-conventions
+// https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/identifier-names
+// https://learn.microsoft.com/en-us/dotnet/standard/design-guidelines/names-of-namespaces
+
+// This BepInAutoPlugin attribute comes from the Hamunii.BepInEx.AutoPlugin
+// NuGet package, and it will generate the BepInPlugin attribute for you!
+// For more info, see https://github.com/Hamunii/BepInEx.AutoPlugin
+[BepInAutoPlugin]
+public partial class Plugin : BaseUnityPlugin
+{
+    internal static ManualLogSource Log { get; private set; } = null!;
+
+    private void Awake()
+    {
+        // BepInEx gives us a logger which we can use to log information.
+        // See https://lethal.wiki/dev/fundamentals/logging
+        Log = Logger;
+
+        // BepInEx also gives us a config file for easy configuration.
+        // See https://lethal.wiki/dev/intermediate/custom-configs
+
+        // We can apply our hooks here.
+        // See https://lethal.wiki/dev/fundamentals/patching-code
+
+        // Log our awake here so we can see it in LogOutput.log file
+        Log.LogInfo($"Plugin {Name} is loaded!");
+    }
+}
